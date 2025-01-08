2cd2e1dbc2960023ddf5ac90b400ba78a8441e45
J Rob Gant <rgant@alum.wpi.edu>
2025-01-07 21:09:26 -0500

    fix typo

--- linter.py
+++ linter.py
@@ -4,7 +4,7 @@ from SublimeLinter.lint import Linter  # or NodeLinter, PythonLinter, ComposerLi
 
 class TerraformLinter(Linter):
     """Subclass to call tflint for linting Terraform HCL files"""
-    cmd = ('tflint', '--chdir=${file_path}' '--filter=${file}')
+    cmd = ('tflint', '--chdir=${file_path}', '--filter=${file}')
     regex = (
         r'^.+?(?:(?P<error>ERROR)|(?P<warning>(?:(WARNING|NOTICE)))):(?P<line>\d+) (?P<message>.+)'
     )
